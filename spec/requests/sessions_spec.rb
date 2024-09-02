49f2cad01d7a6f7fa6e6102dcdf251c2035baa5a
Ruy R. <108208+ruyrocha@users.noreply.github.com>
2024-09-02 16:31:03 -0300

    Require authentication in order to access dashboard and other end-points.

---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe("Sessions") do
+  it "allows users to register"
+  it "allows users to sign in"
+  it "allows users to log out"
+end
---
+++ spec/support/devise.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include(Devise::Test::ControllerHelpers, type: :controller)
+  config.include(Warden::Test::Helpers, type: :system)
+  config.include(Warden::Test::Helpers, type: :request)
+  config.include(Devise::Test::IntegrationHelpers, type: :system)
+  config.include(Devise::Test::IntegrationHelpers, type: :request)
+end
