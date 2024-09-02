bb53ba4476a9854f8384622a98f3a462719b797c
Ruy R. <108208+ruyrocha@users.noreply.github.com>
2024-09-02 13:47:19 -0300

    Initial project files.

---
+++ config/initializers/semantic_logger.rb
@@ -0,0 +1,5 @@
+# Set the global default log level
+SemanticLogger.default_level = :trace
+
+# Log to a file, and use the colorized formatter
+SemanticLogger.add_appender(file_name: "#{Rails.env}-semantic_logger.log", formatter: :color)
