020210b11c447e6f987a5569762c300fbe6413d4
Ruy R. <108208+ruyrocha@users.noreply.github.com>
2024-09-03 11:44:51 -0300

    Fix books view specs.

--- spec/views/books/index.html.tailwindcss_spec.rb
+++ spec/views/books/index.html.tailwindcss_spec.rb
@@ -1,28 +1,32 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "books/index", type: :view do
+require "rails_helper"
+
+RSpec.describe("books/index", type: :view) do
   before(:each) do
+    allow(view).to(receive(:can?).and_return(true))
+
     assign(:books, [
       Book.create!(
         title: "Title",
         author: "Author",
         genre: "Genre",
         isbn: "Isbn",
-        total_copies: 2
+        total_copies: 2,
       ),
       Book.create!(
         title: "Title",
         author: "Author",
         genre: "Genre",
         isbn: "Isbn",
-        total_copies: 2
-      )
+        total_copies: 2,
+      ),
     ])
   end
 
   it "renders a list of books" do
     render
-    cell_selector = 'div>p'
+    cell_selector = "div>p"
     assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Author".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Genre".to_s), count: 2
--- spec/views/books/show.html.tailwindcss_spec.rb
+++ spec/views/books/show.html.tailwindcss_spec.rb
@@ -1,22 +1,26 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "books/show", type: :view do
+require "rails_helper"
+
+RSpec.describe("books/show", type: :view) do
   before(:each) do
+    allow(view).to(receive(:can?).and_return(true))
+
     assign(:book, Book.create!(
       title: "Title",
       author: "Author",
       genre: "Genre",
       isbn: "Isbn",
-      total_copies: 2
+      total_copies: 2,
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Author/)
-    expect(rendered).to match(/Genre/)
-    expect(rendered).to match(/Isbn/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to(match(/Title/))
+    expect(rendered).to(match(/Author/))
+    expect(rendered).to(match(/Genre/))
+    expect(rendered).to(match(/Isbn/))
+    expect(rendered).to(match(/2/))
   end
 end
